037b007322edc50afb7bd2b7e61b003c8620e77a
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-11 10:40:03 +0530

    Update_1 count.java

--- count.java
+++ count.java
@@ -1,19 +1,20 @@
-public class count {
+public class count{
 
 	public static void main(String[] args) {
-		int no = 100;
+		count countobj = new count();
+		countobj.find_divicer(100);
+
+	}
+	private void find_divicer(int no) {
 		int div = 2;
 		int count = 0;
-		
 		while (div < no) {
 			if (no%div == 0) {
 				System.out.println(div);
-				count +=1 ;
+				count += 1;
 			}
 			div += 1;
 		}
 		System.out.println(count);
-
 	}
-
 }
